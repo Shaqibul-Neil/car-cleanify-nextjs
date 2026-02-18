a3803e6c01eeb3fef7d4885267fe574ff447e818
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-02-18 19:35:00 +0600

    revalidate using tags or path for cached items

---
+++ src/app/(withCommonLayout)/reviews/loading.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReviewLoading = () => {
+  return <div>ReviewLoading</div>;
+};
+
+export default ReviewLoading;
--- src/services/reviewsAction.js
+++ src/services/reviewsAction.js
@@ -1,22 +1,36 @@
 "use server";
+
+import { revalidatePath, revalidateTag } from "next/cache";
+
 export const getAllReviews = async (searchParams) => {
   const getParams = new URLSearchParams(searchParams);
   const res = await fetch(
-    `http://localhost:3002/api/review?reviews=${getParams}`,
+    `http://localhost:3000/api/review?reviews=${getParams}`,
+    {
+      //cache: "no-store", //by default
+      cache: "force-cache", //cache krbe
+      //or
+      next: { tags: ["reviews"] },
+    },
   );
 
   const { reviews } = await res.json();
 
-  console.log(reviews);
+  // await new Promise((resolve) => setTimeout(() => resolve(), 3000));
+
   return reviews;
 };
 
 export const createReview = async (payload) => {
-  const res = await fetch(`http://localhost:3002/api/review`, {
+  const res = await fetch(`http://localhost:3000/api/review`, {
     method: "POST",
     headers: { "Content-type": "application/json" },
     body: JSON.stringify(payload),
   });
   if (!res.ok) throw new Error("Failed to create review");
+
+  //revalidatePath("/reviews");
+  //or
+  revalidateTag("reviews");
   return res.json();
 };
